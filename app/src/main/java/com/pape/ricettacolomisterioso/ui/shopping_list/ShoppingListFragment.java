50b40c39eff2a6fdc7c11272f7dea30f08d51309
Mattia Sgro <mattia.sgro@live.it>
2020-04-30 18:18:45 +0200

    added shopping list to navbar

--- app/src/main/java/com/pape/ricettacolomisterioso/ui/shopping_list/ShoppingListFragment.java
+++ app/src/main/java/com/pape/ricettacolomisterioso/ui/shopping_list/ShoppingListFragment.java
@@ -1,4 +1,4 @@
-package com.pape.ricettacolomisterioso.ui.notifications;
+package com.pape.ricettacolomisterioso.ui.shopping_list;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -14,17 +14,17 @@ import androidx.lifecycle.ViewModelProviders;
 
 import com.pape.ricettacolomisterioso.R;
 
-public class NotificationsFragment extends Fragment {
+public class ShoppingListFragment extends Fragment {
 
-    private NotificationsViewModel notificationsViewModel;
+    private ShoppingListViewModel shoppingListViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        notificationsViewModel =
-                ViewModelProviders.of(this).get(NotificationsViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_notifications, container, false);
+        shoppingListViewModel =
+                ViewModelProviders.of(this).get(ShoppingListViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_shoppinglist, container, false);
         final TextView textView = root.findViewById(R.id.text_notifications);
-        notificationsViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+        shoppingListViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
