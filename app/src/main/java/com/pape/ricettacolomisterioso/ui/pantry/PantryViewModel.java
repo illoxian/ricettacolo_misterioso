becd8e4fa36c5a077a857dc7b2e36c219eb693b5
Mattia Sgro <mattia.sgro@live.it>
2020-04-30 18:07:53 +0200

    icons and navbar

--- app/src/main/java/com/pape/ricettacolomisterioso/ui/pantry/PantryViewModel.java
+++ app/src/main/java/com/pape/ricettacolomisterioso/ui/pantry/PantryViewModel.java
@@ -1,16 +1,16 @@
-package com.pape.ricettacolomisterioso.ui.home;
+package com.pape.ricettacolomisterioso.ui.pantry;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class HomeViewModel extends ViewModel {
+public class PantryViewModel extends ViewModel {
 
     private MutableLiveData<String> mText;
 
-    public HomeViewModel() {
+    public PantryViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is home fragment");
+        mText.setValue("This is dashboard fragment");
     }
 
     public LiveData<String> getText() {
--- app/src/main/java/com/pape/ricettacolomisterioso/ui/recipes/RecipesViewModel.java
+++ app/src/main/java/com/pape/ricettacolomisterioso/ui/recipes/RecipesViewModel.java
@@ -1,16 +1,16 @@
-package com.pape.ricettacolomisterioso.ui.dashboard;
+package com.pape.ricettacolomisterioso.ui.recipes;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class DashboardViewModel extends ViewModel {
+public class RecipesViewModel extends ViewModel {
 
     private MutableLiveData<String> mText;
 
-    public DashboardViewModel() {
+    public RecipesViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is dashboard fragment");
+        mText.setValue("This is home fragment");
     }
 
     public LiveData<String> getText() {
