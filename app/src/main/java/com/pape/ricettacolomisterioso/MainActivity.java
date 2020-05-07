60d2b17dc9fd8d0a377529cfac629138b41c36fb
Giuseppe Ziccardi <giuseppe.zick@outlook.it>
2020-05-07 20:58:16 +0200

    Creazione activity inserimento nuovo prodotto

--- app/src/main/java/com/pape/ricettacolomisterioso/MainActivity.java
+++ app/src/main/java/com/pape/ricettacolomisterioso/MainActivity.java
@@ -1,8 +1,11 @@
 package com.pape.ricettacolomisterioso;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -29,6 +32,12 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
+
+        Intent intent = new Intent(getApplicationContext(), ActivityNewProduct.class);
+        startActivity(intent);
+
+
+
     }
 
 }
