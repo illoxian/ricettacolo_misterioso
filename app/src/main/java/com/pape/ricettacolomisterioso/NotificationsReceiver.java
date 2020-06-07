e7a682baa85daa2ee08e3008137c1083ddf9b0ee
Matteo Sangalli <mgango98@gmail.com>
2020-06-07 16:00:14 +0200

    Added notification (partially)

---
+++ app/src/main/java/com/pape/ricettacolomisterioso/NotificationsReceiver.java
@@ -0,0 +1,28 @@
+package com.pape.ricettacolomisterioso;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+import android.widget.Toast;
+
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+public class NotificationsReceiver extends BroadcastReceiver {
+
+    public static String TAG = "NotificationsReceiver";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Log.d(TAG, "onReceive: NotificationSended");
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, "StandardChannel")
+                .setSmallIcon(R.drawable.icon_categories_fish)
+                .setContentTitle("Text notifica")
+                .setContentText("Un bel testo lungo per testare le notifiche")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
+        notificationManager.notify(0, builder.build());
+    }
+
+}
